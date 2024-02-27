2ee66927bd3fd34c9591d91fb7a727ef4ce0b83a
Josh Beller <jrbeller123@gmail.com>
2024-02-26 19:49:49 -0500

    camera thread

--- src/main/java/frc/robot/subsystems/camera/PhotonVisionCamera.java
+++ src/main/java/frc/robot/subsystems/camera/PhotonVisionCamera.java
@@ -1,20 +1,33 @@
 package frc.robot.subsystems.camera;
 
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
 import java.util.function.Consumer;
 
 import org.photonvision.PhotonCamera;
 import org.photonvision.PhotonUtils;
 
+import com.ctre.phoenix6.Utils;
+
 import edu.wpi.first.apriltag.AprilTagFieldLayout;
 import edu.wpi.first.apriltag.AprilTagFields;
+import edu.wpi.first.math.filter.LinearFilter;
+import edu.wpi.first.math.filter.MedianFilter;
+import edu.wpi.first.math.geometry.Pose2d;
 import edu.wpi.first.math.geometry.Pose3d;
 import edu.wpi.first.math.geometry.Rotation3d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation3d;
 import edu.wpi.first.math.util.Units;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class PhotonVisionCamera extends SubsystemBase {
+
+    final double UpdateFrequency = 100.0;
+
+    ReadWriteLock m_stateLock = new ReentrantReadWriteLock();
+
     PhotonCamera camera = new PhotonCamera("3534camera");
     AprilTagFieldLayout aprilTagFieldLayout;
     final Transform3d cameraToRobot = new Transform3d(
@@ -36,4 +49,74 @@ public class PhotonVisionCamera extends SubsystemBase {
             updatePose.accept(robotPose);
         }
     }
+
+    public class CameraState {
+        public Pose3d robotToTarget;
+    }
+
+    final CameraState m_cachedState = new CameraState();
+
+    public class CameraThread {
+        final Thread m_thread;
+        volatile boolean m_running;
+
+        private final MedianFilter peakRemover = new MedianFilter(3);
+        private final LinearFilter lowPass = LinearFilter.movingAverage(50);
+        private double lastTime = 0;
+        private double currentTime = 0;
+        double m_averageLoopTime = 0;
+
+        public CameraThread() {
+            m_thread = new Thread(this::run);
+
+            m_thread.setDaemon(true);
+        }
+
+        /**
+         * Starts the odometry thread.
+         */
+        public void start() {
+            m_running = true;
+            m_thread.start();
+        }
+
+        /**
+         * Stops the odometry thread.
+         */
+        public void stop() {
+            stop(0);
+        }
+
+        /**
+         * Stops the odometry thread with a timeout.
+         *
+         * @param millis The time to wait in milliseconds
+         */
+        public void stop(long millis) {
+            m_running = false;
+            try {
+                m_thread.join(millis);
+            } catch (final InterruptedException ex) {
+                Thread.currentThread().interrupt();
+            }
+        }
+
+        public void run() {
+            while (m_running) {
+                Timer.delay(1.0 / UpdateFrequency);
+                try {
+                    m_stateLock.readLock().lock();
+
+                    lastTime = currentTime;
+                    currentTime = Utils.getCurrentTimeSeconds();
+
+                    m_averageLoopTime = lowPass.calculate(peakRemover.calculate(currentTime - lastTime));
+
+                } finally {
+                    m_stateLock.writeLock().unlock();
+                }
+            }
+        }
+    }
+
 }
